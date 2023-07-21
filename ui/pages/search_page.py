38479ce3d2a16db74f9b056b480ed7374ba24c5c
Ksenia Ivannikova <x.ivannikova@gmail.com>
2023-07-21 17:29:32 +0400

    - Changed test data - Deleted extra logging

--- ui/pages/search_page.py
+++ ui/pages/search_page.py
@@ -18,15 +18,14 @@ class SearchPage:
         main_form = self.driver.find_element(By.ID, 'content')
         results_list = main_form.find_elements(By.CLASS_NAME, 'items')
         ads_list = []
-        logger.info('Start to collect ads_list')
         depth_date = datetime.now() - timedelta(days=depth)
 
+        logger.info('Start to collect ads_list')
         for result in results_list:
             date_field = result.find_element(By.CLASS_NAME, 'dates')
             ad_date = datetime.strptime(date_field.text, "%d.%m.%Y")
             link_css_selector = 'h3 > a'
             if ad_date > depth_date:
-                logger.info(f"link {result.find_element(By.CSS_SELECTOR, link_css_selector).get_attribute('href')}")
                 ads_list.append(Advertisement(ad_type=advertisement.ad_type,
                                               ad_search_string=advertisement.ad_search_string,
                                               ad_date=ad_date.strftime("%d.%m.%Y"),

e23d685574cd4a13d4663df571891774f9934921
XeniaBerkut <x.ivannikova@gmail.com>
2023-07-24 15:38:24 +0400

    - Created config.json to hold some config data and applied into the code - Added method to check if output folder exists - Refactored output writing methods

--- helpers/output_helpers.py
+++ helpers/output_helpers.py
@@ -1,31 +1,63 @@
 import xlsxwriter
 from ui.entities.advertisement import Advertisement
+import os
 
 
-def write_ads_search_result_to_excel(advertisement_list: list[Advertisement], file_name):
-    with xlsxwriter.Workbook(f'./azdarar_search_results/{file_name}.xlsx') as workbook:
+def create_directory_for_results(output_folder, parent_folder):
+    if not os.path.isdir(parent_folder):
+        os.mkdir(parent_folder)
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+
+
+def convert_ads_to_list_of_lists(advertisement_list):
+    list_of_lists = []
+    for ad in advertisement_list:
+        temp = [ad.ad_date, ad.ad_link, ad.ad_text]
+        list_of_lists.append(temp)
+    return list_of_lists
+
+
+def write_ads_search_result_to_excel(advertisement_list: list[Advertisement], file_name, output_folder):
+    output_folder_excel = output_folder + 'excel/'
+    create_directory_for_results(output_folder_excel, output_folder)
+    advertisement_list_xlsx_file_path = output_folder_excel + file_name + '.xlsx'
+    with xlsxwriter.Workbook(advertisement_list_xlsx_file_path) as workbook:
         worksheet = workbook.add_worksheet()
-
-        # for row_num, ad in enumerate(advertisement_list):
-        #     worksheet.write_row(row_num, 1, ad.ad_date)
-        #     worksheet.write_row(row_num, 2, ad.ad_link)
-        #     worksheet.write_row(row_num, 3, ad.ad_text)
-        row_num = 1
-        for ad in advertisement_list:
-            worksheet.write_row(row_num, 1, ad.ad_date)
-            worksheet.write_row(row_num, 2, ad.ad_link)
-            worksheet.write_row(row_num, 3, ad.ad_text)
-            row_num += 1
-
+    converted_ads_list = convert_ads_to_list_of_lists(advertisement_list)
+    for row_num, data in enumerate(converted_ads_list):
+        worksheet.write_row(row_num, 0, data)
     workbook.close()
 
 
-def write_ads_search_result_to_html(advertisement_list: list[Advertisement], file_name):
-    ads_html_file = open(f"./azdarar_search_results/{file_name}.html", "w", encoding="utf-8")
+def write_ads_search_result_to_html(advertisement_list: list[Advertisement], file_name, output_folder):
+    output_folder_html = output_folder + 'html/'
+    create_directory_for_results(output_folder_html, output_folder)
+    advertisement_list_html_file_path = output_folder_html + file_name + '.html'
+    ads_html_file = open(advertisement_list_html_file_path, "w", encoding="utf-8")
     ad_number = 1
+    html_text = """
+    <html>
+        <head>
+            <title> Azdarar Smart Search </title>
+        </head>
+        <body>
+        """
     for ad in advertisement_list:
-        ads_html_file.write(f'<html>\n<head>\n<title> \n \
-               </title>\n</head> <body><h1 style="background-color:DodgerBlue;">{ad_number}. Search string <b> {ad.ad_search_string} </b> <br> <a href="{ad.ad_link}">{ad.ad_link}</a> <br></h1>\
-               \n<h2> {ad.ad_text}</u> </h2> <br> \n</body></html>')
+        html_text += f"""
+        <h1 style="background-color:#00b8e6;">{ad_number}. Search string <b>{ad.ad_search_string}</b>
+        <br>
+            <a href="{ad.ad_link}">{ad.ad_link}</a>
+            <br>
+        </h1>
+        <br>
+        <h2>{ad.ad_text}</h2>
+        <br>
+        """
         ad_number += 1
+    html_text += """
+        </body>
+    </html>
+    """
+    ads_html_file.write(html_text)
     ads_html_file.close()
--- ui/tests/test_search_home_page.py
+++ ui/tests/test_search_home_page.py
@@ -6,23 +6,23 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from ui.pages.home_page import HomePage
 from ui.pages.search_page import SearchPage
 from ui.pages.advertisement_page import AdvertisementPage
-from helpers.output_helpers import write_ads_search_result_to_excel
-from helpers.output_helpers import write_ads_search_result_to_html
+from helpers.output_helpers import write_ads_search_result_to_excel, write_ads_search_result_to_html
 from ui.entities.advertisement import Advertisement
 
 
-def get_data() -> list[dict]:
+def get_data(file_name) -> list[dict]:
     json_path = os.path.join(os.path.dirname(__file__),
-                             "test_data_search_advertisement.json")
+                             file_name)
     with open(json_path, encoding='utf-8') as json_file:
         data = json.load(json_file)
     return data
 
 
-@pytest.mark.parametrize("advertisement_data", get_data())
+@pytest.mark.parametrize("advertisement_data", get_data("test_data_search_advertisement.json"))
 def test_search(driver: WebDriver, advertisement_data: dict):
 
     advertisement = Advertisement(**advertisement_data)
+    test_config = get_data("test_data_search_config.json")
 
     home_page: HomePage = HomePage(driver)
     search_page: SearchPage = SearchPage(driver)
@@ -30,15 +30,17 @@ def test_search(driver: WebDriver, advertisement_data: dict):
     logging.info(f'Start search with search data {advertisement.ad_search_string}')
     home_page.search_home_page(advertisement.ad_search_string)
     logging.info('Collect advertisements links for specified search data')
-    advertisements_list = search_page.collect_results(advertisement, depth=7)
+    advertisements_list = search_page.collect_results(advertisement, test_config['search_date_depth'])
     logging.info('Go to advertisements pages and collect advertisements outerTexts')
     for ad in advertisements_list:
         ad_page.go_to_ad_page(ad.ad_link)
         ad_text = ad_page.get_ad_text()
         ad.ad_text = ad_text
     logging.info('Finished to collect advertisements texts')
+    logging.info('Check if directory for output exist and create if not')
+    output_folder_name = test_config['output_folder']
     logging.info('Write results to excel')
-    write_ads_search_result_to_excel(advertisements_list, advertisement.ad_type)
+    write_ads_search_result_to_excel(advertisements_list, advertisement.ad_type, output_folder_name)
     logging.info('Write results to html file')
-    write_ads_search_result_to_html(advertisements_list, advertisement.ad_type)
+    write_ads_search_result_to_html(advertisements_list, advertisement.ad_type, output_folder_name)
     assert advertisements_list != []
